7a289200b8eb2ee5ada43e13af50a5885d47802b
JulianneSilawan <100115102+JulianneSilawan@users.noreply.github.com>
2022-02-21 15:46:31 +0800

    Update index.php

--- LT2/index.php
+++ LT2/index.php
@@ -9,7 +9,7 @@
 	<h1>Hello, I am Julianne Palermo Silawan, this is my first web page.</h1>
 	
 	<ul>
-		<li><a href="index.html">HOME</a></li>
+		<li><a href="index.php">HOME</a></li>
 		<li><a href="/LT2/index.html">LEARNING TASKS</a></li>
 	</ul>
 </body>
