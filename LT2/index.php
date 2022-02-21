29b20d9fd3375cbc3872f88653da288b617e7052
JulianneSilawan <100115102+JulianneSilawan@users.noreply.github.com>
2022-02-21 15:06:45 +0800

    Create index.php

---
+++ LT2/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<title>
+	My First Website
+	</title>
+	</head>
+	<body>
+	<h1>Hello, I am Julianne Palermo Silawan, this is my first web page.</h1>
+	
+	<ul>
+		<li><a href="index.html">HOME</a></li>
+		<li><a href="/LT2/index.html">LEARNING TASKS</a></li>
+	</ul>
+</body>
+</html>
