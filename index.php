b857ced1d5ae5d7b0136918804de27cdbd76b020
JulianneSilawan <100115102+JulianneSilawan@users.noreply.github.com>
2022-03-21 22:05:31 +0800

    Update index.php

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
 		<li><a href="/index.php">HOME</a></li>
 		<li><a href="/LT2/index.html">LEARNING TASKS 2</a></li>
 		<li><a href="/LT3/index.html">LEARNING TASKS 3</a></li>
+		<li><a href="/CLE1MT/index.html">CLE1MT</a></li>
 	</ul>
 </body>
 </html>
