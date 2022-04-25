fe2f28b127301a81f8a31cd0dc1a288e510002e3
JulianneSilawan <100115102+JulianneSilawan@users.noreply.github.com>
2022-04-25 10:16:33 +0800

    Add files via upload

---
+++ LT5/java.js
@@ -0,0 +1,20 @@
+function clickEvent(obj) {
+	obj.innerHTML = "Hope you have a nice and fun day <3";
+	
+
+}
+
+function hoverEvent(obj){
+	obj.innerHTML = "ENHYPEN!";
+	
+}
+
+function unHoverEvent(obj){
+	obj.innerHTML = "ENHYPEN";
+}
+
+function downEvent(obj){
+	obj.innerHTML = "WE LOVE YOU!!";
+
+
+	}
